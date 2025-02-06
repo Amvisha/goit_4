74e1a774ddc9acf3ee62dcebf7b133b559cfa8a8
Amvisha <amvisha@gmail.com>
2025-02-07 00:43:56 +0200

    1

--- files_mod_4.py
+++ files_mod_4.py
@@ -1,1 +1,13 @@
+# open(file, mode='r', buffering=-1, encoding=None, errors=None, newline=None, closefd=True, opener=None)
+
+fh = open('test.txt', 'w+')
+fh.write('hello!')
+fh.seek(0)
+
+first_two_symbols = fh.read(2)
+print(first_two_symbols)  # 'he'
+
+fh.close()
+
+
 
